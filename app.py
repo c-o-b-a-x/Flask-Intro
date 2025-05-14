a009217664266d1fa4935196d96fc724e9b63a78
c-o-b-a-x <cobyhughes2008@gmail.com>
2025-05-14 08:13:35 -0400

    cats!

--- app.py
+++ app.py
@@ -22,6 +22,7 @@
 #import flask class from flask 
 from flask import Flask,render_template,request
 import datetime
+import requests
 app=Flask(__name__)
 
 #define route for home page
@@ -54,6 +55,24 @@ def  solve():
         operation=request.form.get('operation')
         answer = eval(f"{int(num1)}{operation}{int(num2)}")
         return render_template ("math_results.html",answer=answer,num1=num1,num2=num2,operation=operation)
+
+@app.route ("/cat-fact")
+def catfact():
+        response=requests.get("https://catfact.ninja/fact")
+        if response.status_code== 200:
+                data = response.json()
+                fact=data["fact"]
+        else:
+                fact = "Sorry I couldnt get a cat fact right now please try again later "
+
+        picsrep= requests.get("https://cataas.com/cat?json=true")
+        if response.status_code== 200:
+                picdata = picsrep.json()
+                img=picdata["url"]
+        else:
+                img = "/image/404.png"
+        return render_template("cat.html",cat_fact=fact,cat_img= img)
+
 
 
 if __name__ == "__main__":
